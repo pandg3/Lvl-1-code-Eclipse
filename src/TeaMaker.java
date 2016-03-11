41f50bb15b2867bee488884204b1cd9db4c2be71
Mrfuzzy <league@WTS9.attlocal.net>
2016-03-10 16:59:40 -0800

    stuffs

---
+++ src/TeaMaker.java
@@ -0,0 +1,73 @@
+
+
+// Copyright Wintriss Technical Schools 2013
+public class TeaMaker {
+	
+	
+	public static void main(String[] args) {
+	TeaBag tea = new TeaBag("Green");
+	Kettle ketle = new Kettle();
+	Cup trumpz = new Cup();
+		ketle.getWater();
+		ketle.boil();
+		trumpz.makeTea(tea, ketle.getWater());
+	}
+}
+
+class TeaBag {
+
+	public final static String GREEN = "Green";
+	public final static String MINT = "Mint";
+	public final static String CHAMOMILE = "Chamomile";
+	public final static String PASSION_FRUIT = "Passion Fruit";
+
+	private String flavor;
+
+	TeaBag(String flavor) {
+		this.flavor = flavor;
+	}
+
+	String getFlavor() {
+		return flavor;
+	}
+
+}
+
+class Kettle {
+
+	private Water water = new Water();
+
+	Water getWater() {
+		return water;
+	}
+
+	void boil() {
+		this.water.isHot = true;
+	}
+
+	class Water {
+
+		private boolean isHot = false;
+
+		public boolean isHot() {
+			return this.isHot;
+		}
+	}
+
+}
+
+class Cup {
+
+	private TeaBag teabag;
+
+	void makeTea(TeaBag teabag, Kettle.Water hotWater) {
+		this.teabag = teabag;
+		if (hotWater.isHot())
+			System.out.println("Making hot " + teabag.getFlavor() + " tea.");
+		else
+			System.out.println("Can't make tea with cold water! ");
+	}
+
+}
+
+
---
+++ src/name.java
@@ -0,0 +1,32 @@
+
+public class name {
+	static String text = "Trumpalicious";
+	int jump = 3;
+	public static void main (String[] args){
+		System.out.println(text);
+		
+		if (text.equals("Trumpalicious")){
+																																				System.err.println("\n Error: Server Logestic entity has invalid return code. Return code should of been 'Hight', but return returned 'Politics are too hard for this computer to handle'");
+	}	
+	
+			
+			}
+		
+		
+	}
+
+	class jump{
+		private int jump;
+
+		public jump(int jump){
+			this.jump = jump;
+			if (jump <5){
+				System.out.println();
+				
+			}
+		}
+	}
+
+
+
+
